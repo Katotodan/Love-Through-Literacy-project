d24300cd143b602ac987f26301e0cd76b8fcafdd
Katotodan <katotodan@gmail.com>
2022-09-25 21:33:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// header paragraph script
+const headerPara = document.querySelector('.header_paragraph')
+const paragraphWord = [
+    'Love Though Literaracy Project', 'Insparing Readers One Word at a Time!',
+    'English Language & Literacy Development Center, Ltd'
+]
+let index = 0;
+setInterval(() => {
+    index++
+    if(index === 3){
+        index = 0
+    }
+    headerPara.innerHTML = ''
+    setTimeout(() => {
+        headerPara.innerHTML = paragraphWord[index]
+    }, 500);
+}, 4000);
