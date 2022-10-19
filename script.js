5109ee656c34c8e935ade7473d040eb71915e0e6
Katotodan <katotodan@gmail.com>
2022-10-19 14:09:52 +0300

    responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // header paragraph script
-const headerPara = document.querySelector('.header_paragraph')
+const headerPara = document.querySelector('.word')
 const paragraphWord = [
     'Love Though Literaracy Project', 'Inspiring Readers One Word at a Time!',
     'English Language & Literacy Development Center, Ltd'
@@ -13,5 +13,45 @@ setInterval(() => {
     headerPara.innerHTML = ''
     setTimeout(() => {
         headerPara.innerHTML = paragraphWord[index]
+        document.querySelector('#word').innerHTML = paragraphWord[index]
     }, 500);
 }, 4000);
+// Script for menu responsiveness
+const menu = document.querySelector('.menu')
+const navMenu = document.querySelector('.top_nav--ul div')
+const deletebtn = document.querySelector('.delete')
+window.onresize = () => {
+    if(document.querySelector('body').clientWidth > 834){
+        navMenu.style.display = 'flex'
+        menu.style.visibility = 'visible'
+        deletebtn.style.visibility = 'hidden'
+    }else{
+        navMenu.style.display = 'none'
+        
+    }
+}
+const menuRemove = () =>{
+    deletebtn.style.visibility = 'hidden'
+    navMenu.style.animationName = 'end'
+    setTimeout(() => {
+        navMenu.style.display = 'none'
+        navMenu.style.animationName = 'start'
+        menu.style.visibility = 'visible'
+    }, 3000);
+}
+document.querySelector('.header_img').addEventListener('click', () =>{
+    if(menu.style.visibility === "hidden"){
+        menuRemove()
+    }
+})
+menu.addEventListener('click', () =>{
+    navMenu.style.display = 'flex'
+    menu.style.visibility = 'hidden'
+    setTimeout(() => {
+        deletebtn.style.visibility = 'visible'
+    }, 3000);
+})
+deletebtn.addEventListener('click',() =>{
+    menuRemove()   
+})
+
